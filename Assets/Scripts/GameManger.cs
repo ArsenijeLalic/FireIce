a41c4ae2ff6f87fc3f54e5ec5bc2d2db09071919
Arsenije Lalić <arsenijelalic80@gmail.com>
2024-03-31 22:21:38 +0200

    Exit and Restart implemented

---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
